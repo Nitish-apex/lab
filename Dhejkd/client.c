dad604331871022728027b37eaa5e24c6b19b145
Nitish <55088665+Nitish-apex@users.noreply.github.com>
2020-02-29 07:58:30 +0530

    Update client.c

--- Dhejkd/client.c
+++ Dhejkd/client.c
@@ -1,1 +1,8 @@
-
+#include<stdio.h>
+int main(){
+  char o,n,x[100]
+  printf("enter a string broo\n");
+  scanf("%s\n",x);
+  printf("enter char to be replaced and replaced by\n");
+  scanf("%c %c",&o,&n);
+  
