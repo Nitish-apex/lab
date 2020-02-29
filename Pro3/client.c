bec61f6ca423323c76f63e8d0abf33d32424d12a
Nitish <55088665+Nitish-apex@users.noreply.github.com>
2020-02-29 07:37:11 +0530

    Update client.c

--- Pro3/client.c
+++ Pro3/client.c
@@ -1,1 +1,12 @@
+#include<stdio.h>
+#include "header.h"
+int main(){
+  char x[100],y[20];
+  printf("enter string 1\n");
+  scanf("%s\n",x);
+  printf("enter string 2\n");
+  scanf("%s\n",y);
+  printf("enter no of concats\n);
+  scanf("%d\n",&d);   
+ }
 
